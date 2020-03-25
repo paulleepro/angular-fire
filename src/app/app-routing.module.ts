9bb3a7a3524eafc54193135ae6de4bb21ff4c89c
Paul Lee <plee6950@gmail.com>
2020-03-25 17:20:04 -0400

    initial project setup - simple nav and courselist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+
+  { path: "welcome", component: WelcomeComponent },
+  //  { path: "about", component: AboutComponent },
+
+  {
+    path: "about", component: AboutComponent
+  },
+
+  // Before Angular 8
+  // { path: "about", loadChildren: './about/about.module#AboutModule' },
+
+
+
+  // { path: "albums", component: AlbumListComponent },
+  {
+    path: "courses", //Angular 8 Notation with Promise
+    component: CoursesListComponent
+    // canActivate: [AuthGuard],
+    // loadChildren: () => import('./albums/albums.module')
+    //   .then(mod => {
+    //     console.log('in promise loadChildren');
+    //     return mod.AlbumsModule;
+    //   }),
+  },
+  // //Before Angular 8
+  // { path: "observables", loadChildren: './observables/observables.module#ObservablesModule' },
+  { path: "**", component: NotfoundComponent, pathMatch: "full" }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// tslint:disable-next-line: quotemark
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NotfoundComponent } from './notfound/notfound.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // AboutComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    CoursesListComponent,
+    WelcomeComponent,
+    LoginComponent,
+    AboutComponent,
+    // WelcomeComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // ,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
