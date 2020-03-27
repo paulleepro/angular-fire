04446317f6d5fb87f230c97624e04ac81220db63
Paul Lee <plee6950@gmail.com>
2020-03-27 14:11:42 -0400

    updated to use options

--- src/app/shared/course.ts
+++ src/app/shared/course.ts
@@ -1,11 +1,12 @@
 export interface Course {
   id: number;
-  "title": string;
-  "duration": number;
-  "duration_unit": string;
-  "short_description": string;
-  "description": string;
-  "overview": string;
-  "image_url": string;
-  "authors": string[];
+  title: string;
+  duration: number;
+  duration_unit: string;
+  options?: string;
+  short_description: string;
+  description: string;
+  overview: string;
+  image_url: string;
+  authors: string[];
 }
