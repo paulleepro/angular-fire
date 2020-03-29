735f636e92f9fd68cdf32ab4b5efec8ab4f44768
Paul Lee <plee6950@gmail.com>
2020-03-29 21:12:32 +0200

    add category for course and some fields

--- src/app/courses-list/courses-list.component.ts
+++ src/app/courses-list/courses-list.component.ts
@@ -4,27 +4,33 @@ import { CourseService } from '../shared/course.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
 import { Observable } from 'rxjs';
-import {tap} from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-courses-list',
-  templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.css']
+	selector: 'app-courses-list',
+	templateUrl: './courses-list.component.html',
+	styleUrls: [ './courses-list.component.css' ]
 })
 export class CoursesListComponent implements OnInit {
+	courses: Course[];
+	starterCourse: Course[];
+	frontEndCourse: Course[];
+	backEndCourse: Course[];
 
-  courses: Course[];
+	constructor(private service: CourseService) {}
 
-  constructor(private service: CourseService) {  }
-
-  ngOnInit(): void {
-    this.service.getCourses()
-      .subscribe({
-          next: (courses: Course[]) => {
-            this.courses = courses;
-          }
-    });
-  }
-
-
+	ngOnInit(): void {
+		this.service.getCourses().subscribe({
+			next: (courses: Course[]) => {
+				this.courses = courses;
+				this.starterCourse = this.courses.filter((course) => course.category === 'starter');
+				this.frontEndCourse = this.courses.filter(
+					(course) => course.category === 'front-end'
+				);
+				this.backEndCourse = this.courses.filter(
+					(course) => course.category === 'back-end'
+				);
+			}
+		});
+	}
 }
