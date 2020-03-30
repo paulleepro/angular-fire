371f928891c317e42547526bef3ae8cd19cffd36
Paul Lee <plee6950@gmail.com>
2020-03-30 23:36:02 +0200

    delete the logic for side menue in courses page

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -7,30 +7,23 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-courses-list',
-  templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.css']
+	selector: 'app-courses-list',
+	templateUrl: './courses-list.component.html',
+	styleUrls: [ './courses-list.component.css' ]
 })
 export class CoursesListComponent implements OnInit {
-  courses: Course[];
-  starterCourse: Course[];
-  frontEndCourse: Course[];
-  backEndCourse: Course[];
+	courses: Course[];
+	starterCourse: Course[];
+	frontEndCourse: Course[];
+	backEndCourse: Course[];
 
-  constructor(private service: CourseService) { }
+	constructor(private service: CourseService) {}
 
-  ngOnInit(): void {
-    this.service.getCourses().subscribe({
-      next: (courses: Course[]) => {
-        this.courses = courses;
-        this.starterCourse = this.courses.filter((course) => course.category === 'starter');
-        this.frontEndCourse = this.courses.filter(
-          (course) => course.category === 'front-end'
-        );
-        this.backEndCourse = this.courses.filter(
-          (course) => course.category === 'back-end'
-        );
-      }
-    });
-  }
+	ngOnInit(): void {
+		this.service.getCourses().subscribe({
+			next: (courses: Course[]) => {
+				this.courses = courses;
+			}
+		});
+	}
 }
